b5663d6c03325a7570f8620919cbb26e00e01ffb
goforpordev <goforprodev@gmail.com>
2022-07-28 14:43:03 +0100

    Added showcase component

---
+++ components/ShowCase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/ShowCase.module.css'
+
+export default function ShowCase(){
+    return(
+        <div className={styles.showcase}>
+            <h1>Welcome to the party!</h1>
+            <h1>Welcome to Afro Events get latest events and parties</h1>
+        </div>
+    )
+}
